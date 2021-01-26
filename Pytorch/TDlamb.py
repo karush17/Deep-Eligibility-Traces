86b24ca8d82b1985dbe3b640a10ec106db16a70c
karush17 <karushsuri@gmail.com>
2021-01-26 09:42:58 -0500

    update tdlambda trace initialization

--- Pytorch/TDlamb.py
+++ Pytorch/TDlamb.py
@@ -20,27 +20,39 @@ class TDLambda(nn.Module):
         self.opt_actor = torch.optim.Adam(self.actor.parameters(), lr=args.lr)
         self.opt_value = torch.optim.Adam(self.value_net.parameters(), lr=args.lr)
         self.trace = {}
+        for idx, p in enumerate(self.actor.parameters()):
+            self.trace[idx] = torch.zeros(p.data.shape).to(DEVICE)
         print(self.actor)
         print(self.opt_value)
     
     def forward(self, states):
         return self.actor(states)
     
-    def get_actions(self, states):
-        return self.actor.get_actions(states)
-    
-    def update(self, args, states, reward, next_states, done, step_count):
-        self.opt_actor.zero_grad()
+    def get_actions(self, steps, states):
+        return self.actor.get_actions(steps, states)
+
+    def update(self, replay_buffer, states, reward, next_states, done, step_count):
+        batch_size = self.args.batch_size
+        states, actions, rewards, next_states, dones = replay_buffer.sample(batch_size)
+        states = to_torch(states)
+        actions = to_torch(actions).type(torch.int64)
+        rewards = to_torch(rewards)
+        next_states = to_torch(next_states)
+        dones = to_torch(dones)
         vals = self.actor(states)#[self.actor.get_actions(states)]
-        next_vals = to_np(args, self.actor(next_states))
+        vals = vals.gather(1, actions.unsqueeze(1)).squeeze(1)
+        next_vals = self.actor(next_states)
+        new_next_vals = torch.zeros([self.args.batch_size,1], dtype=torch.float32).to(DEVICE)
+        for i in range(len(next_vals)):
+            new_next_vals[i] = to_torch(np.random.choice(to_np(self.args, next_vals[i,:]), p=to_np(self.args, F.softmax(next_vals[i,:]))))
         # print(vals)
         # print(next_vals)
         # next_vals = np.random.choice(next_vals, 1)[0]
-        target = reward + self.args.gamma*next_vals*(1 - done)
-        td_error = torch.mean((to_torch(target).detach() - vals)**2)
-        # td_error.backward()
+        target = reward + self.args.gamma*new_next_vals*(1 - dones)
+        td_error = (target.detach() - vals).pow(2).mean()
+        self.opt_actor.zero_grad()
         self.trace = self.reset_trace(step_count) 
-        eval_gradients = ag.grad(vals[self.actor.get_actions(states)], self.actor.parameters())
+        eval_gradients = ag.grad(vals.mean(), self.actor.parameters())
         for idx, p in enumerate(self.actor.parameters()):
             self.trace[idx] = self.args.gamma*self.args.lamb*self.trace[idx] + eval_gradients[idx]
             p.grad = -td_error*self.trace[idx]
