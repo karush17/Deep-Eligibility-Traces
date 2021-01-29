3abe2b8b342690103af9222402f58cde37893273
karush17 <karushsuri@gmail.com>
2021-01-28 21:02:26 -0500

    add dutch trace

--- traces/traces.py
+++ traces/traces.py
@@ -2,17 +2,19 @@ import os
 import numpy as np
 import torch
 
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
 def replacing(args, actions, trace):
     trace = trace.scatter_(1, actions.unsqueeze(1), 1)
     return trace
 
 def accumulating(args, actions, trace):
-    upd = torch.zeros(trace.shape).to(DEVICE)
-    trace += upd.scatter_(1, actions.unsqueeze(1), 1)
+    trace = trace.scatter_(1, actions.unsqueeze(1), 1, reduce='add')
     return trace
 
-def dutch(args, state, trace):
-    trace = args.gamma*args.lamb*trace + (1 - args.alpha*args.gamma*args.lamb*trace.T*state)*state
+def dutch(args, actions, trace):
+    trace = trace.scatter_(1, actions.unsqueeze(1), 1-args.alpha, reduce='multiply')
+    trace = trace.scatter_(1, actions.unsqueeze(1), 1, reduce='add')
     return trace
 
 
