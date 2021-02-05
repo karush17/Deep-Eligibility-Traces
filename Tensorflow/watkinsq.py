55bae469090313c3f08512406be08159700026d2
karush17 <karushsuri@gmail.com>
2021-02-05 12:12:43 -0500

    add watkinsq lambda tf

--- Tensorflow/watkinsq.py
+++ Tensorflow/watkinsq.py
@@ -1,31 +1,30 @@
 import os
-import random
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import optimizers
 from networks.tf_networks import *
 from utils.utils import *
-import traces
 
-
-class SARSA(tf.Module):
+class WatkinsQ(tf.Module):
     def __init__(self, args, state_dims, num_actions):
-        super(SARSA, self).__init__()
+        super(WatkinsQ, self).__init__()
         self.args = args
         self.state_dims = state_dims
         self.num_actions = num_actions
         self.actor = ActorNetwork(args, state_dims, num_actions)
         self.opt_actor = optimizers.Adam(learning_rate=self.args.lr)
-        self.trace = tf.zeros((self.args.batch_size, self.num_actions))
+        self.trace = {}
+        for idx, p in enumerate(self.actor.trainable_variables):
+            self.trace[idx] = tf.zeros(p.get_shape())
         print(self.actor)
         print(self.opt_actor)
     
-    def __call__(self, states):
+    def forward(self, states):
         return self.actor(states)
     
     def get_actions(self, steps, states):
         return self.actor.get_actions(steps, states)
-    
+
     def update(self, replay_buffer, steps, step_count):
         batch_size = self.args.batch_size
         states, actions, rewards, next_states, dones = replay_buffer.sample(batch_size)
@@ -35,31 +34,25 @@ class SARSA(tf.Module):
         next_states = to_tensor(next_states)
         dones = to_tensor(dones)
         with tf.GradientTape() as tape:
-            vals = self.actor(states)#[self.actor.get_actions(states)]
+            vals = self.actor(states)
             vals = tf.gather_nd(params=vals, indices=tf.transpose(actions), batch_dims=1)
-            next_vals = self.actor(next_states)
-            next_actions = tf.cast(self.actor.get_actions(steps, next_states), tf.int64)
-            next_vals = tf.gather_nd(params=next_vals, indices=tf.transpose(next_actions), batch_dims=1)
+            next_vals = tf.reduce_max(self.actor(next_states), axis=1)
             target = tf.stop_gradient(rewards + self.args.gamma*next_vals*(1 - dones))
             td_error = (target - vals)**2
-            # trace update
-            if self.args.trace!='none':
-                if step_count==0:
-                    self.trace = self.reset_trace()
-                self.trace  = self.update_trace(actions)
-                td_error *= tf.gather_nd(params=self.trace, indices=tf.transpose(actions), batch_dims=1)
-                self.trace *= self.args.gamma*self.args.lamb
             td_error = tf.reduce_mean(td_error, axis=1)
-            # td update
+            self.trace = self.reset_trace(step_count) 
             grads = tape.gradient(td_error, self.actor.trainable_variables)
+            for idx, p in enumerate(self.actor.trainable_variables):
+                self.trace[idx] = self.args.gamma*self.args.lamb*self.trace[idx] + grads[idx]
+                grads[idx] = td_error*self.trace[idx]
             self.opt_actor.apply_gradients(zip(grads, self.actor.trainable_variables))
         return to_np(self.args, td_error)[0]
 
-    def update_trace(self, actions):
-        return getattr(torch_traces, self.args.trace)(self.args, actions, self.trace)
-
-    def reset_trace(self):
-            return tf.zeros((self.args.batch_size, self.num_actions))
+    def reset_trace(self, step_count):
+        if step_count==1:
+            for idx, p in enumerate(self.actor.trainable_variables):
+                self.trace[idx] = tf.zeros(p.get_shape())
+        return self.trace
 
 
 
