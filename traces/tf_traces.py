a75fa377c8c34dc99e155d0d7533ef8c56e2ea85
karush17 <karushsuri@gmail.com>
2021-02-08 13:45:30 -0500

    tf traces complete

--- traces/tf_traces.py
+++ traces/tf_traces.py
@@ -2,6 +2,8 @@ import os
 import numpy as np
 import tensorflow as tf
 
+# one could also use the tf.tensor_scatter_nd_update function for updating the trace inplace. However, this requires a 2 dimensional action index tensor which might be difficult to maintina during training. 
+
 def replacing(args, actions, trace):
     trace = tf.one_hot(tf.squeeze(tf.transpose(actions),1), trace.get_shape()[1])
     return trace
